d9890c71315ebe6c9d69c95d8e80e7b27b52211a
Chris Cheetham <chris@cheetham.com>
2020-06-25 15:42:12 -0400

    Initial setup

---
+++ src/test/java/io/steeltoe/initializr/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.steeltoe.initializr.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
