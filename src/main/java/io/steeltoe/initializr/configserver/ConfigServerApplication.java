f19da11d0f2390d63a14acd0211d2ac135abe2af
Chris Cheetham <chris@cheetham.com>
2020-06-29 17:52:29 -0400

    Log "about" at startup [Closes #5]

--- src/main/java/io/steeltoe/initializr/configserver/ConfigServerApplication.java
+++ src/main/java/io/steeltoe/initializr/configserver/ConfigServerApplication.java
@@ -4,16 +4,43 @@
 
 package io.steeltoe.initializr.configserver;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 @EnableConfigServer
 @SpringBootApplication
-public class ConfigServerApplication {
+public class ConfigServerApplication implements ApplicationContextAware {
+
+    private static final Logger logger = LoggerFactory.getLogger(ConfigServerApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(ConfigServerApplication.class, args);
     }
 
+    @Override
+    public void setApplicationContext(ApplicationContext context) throws BeansException {
+        BuildProperties buildProperties = context.getBean(BuildProperties.class);
+        InputStream pin = getClass().getClassLoader().getResourceAsStream("git.properties");
+        Properties gitProperties = new Properties();
+        if (pin != null) {
+            try {
+                gitProperties.load(pin);
+                pin.close();
+            } catch (IOException e) {
+                logger.warn("IO error processing Git properties: {}", e.getLocalizedMessage());
+            }
+        }
+        logger.info("{}, version {} [{}]", buildProperties.getName(), buildProperties.getVersion(), gitProperties.getOrDefault("git.commit.id", "unknown"));
+    }
 }
